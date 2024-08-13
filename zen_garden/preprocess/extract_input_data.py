f0663a772d0e3d0a7a2885b641699bf987208f3c
johburger <63449992+johburger@users.noreply.github.com>
2024-08-13 08:39:56 +0200

    performance test 4

--- zen_garden/preprocess/extract_input_data.py
+++ zen_garden/preprocess/extract_input_data.py
@@ -146,25 +146,27 @@ class DataInput:
             else:
                 index_multi_index = pd.MultiIndex.from_product([index_list], names=[df_input.index.name])
             df_input = pd.Series(index=index_multi_index, data=df_input.to_list(),dtype=float)
-        if file_name == 'operation_state':
+        if file_name == 'operation_state' and self.element.name == 'train':
 
             t0 = time.time()  # perf_counter()
             tp0 = time.perf_counter()
+
             common_index = df_output.index.intersection(df_input.index)
             df_output.loc[common_index] = df_input.loc[common_index]
+
             logging.info('index.intersection')
             logging.info(f'time: {(time.time() - t0)}')
             logging.info(f'Perftime: {(time.perf_counter() - tp0)}')
-
+        elif file_name == 'operation_state' and self.element.name == 'pipeline':
             t0 = time.time()#perf_counter()
             tp0 = time.perf_counter()
-            tmp = df_input[df_input.index.get_level_values(1).isin(self.energy_system.set_failures)].copy()
-            tmp_out = df_output.copy()
-            tmp_out[tmp.index] = tmp
+
+            df_input = df_input[df_input.index.get_level_values(1).isin(self.energy_system.set_failures)].copy()
+            df_output[df_input.index] = df_input
+
             logging.info('.get_level_values().isin()')
             logging.info(f'time: {(time.time() - t0)}')
             logging.info(f'Perftime: {(time.perf_counter() - tp0)}')
-            print('done')
         else:
             common_index = df_output.index.intersection(df_input.index)
             assert default_value is not None or len(common_index) == len(df_output.index), f"Input for {file_name} does not provide entire dataset and no default given in attributes.csv"
